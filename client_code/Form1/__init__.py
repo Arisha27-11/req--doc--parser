381e5ef95bd3d7e81f2ee9533b0eeacc780b7e76
arisha.masood@gmail.com <arisha.masood@gmail.com>
2024-06-02 21:22:15 +0000

    Created ServerModule1, Form1 and LICENSE.txt; Edited settings

---
+++ client_code/Form1/__init__.py
@@ -0,0 +1,16 @@
+from ._anvil_designer import Form1Template
+from anvil import *
+import stripe.checkout
+import anvil.server
+
+
+class Form1(Form1Template):
+  def __init__(self, **properties):
+    # Set Form properties and Data Bindings.
+    self.init_components(**properties)
+
+    # Any code you write here will run before the form opens.
+
+  def file_loader_1_change(self, file, **event_args):
+    """This method is called when a new file is loaded into this FileLoader"""
+    pass
